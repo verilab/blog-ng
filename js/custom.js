125b9164a4f70694c1571e2c31acbcaad153d257
Richard Chien <richardchienthebest@gmail.com>
2016-08-04 18:25:55 +0800

    Improve music player

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-var handleResize = function (event) {
+function handleResize(event) {
     var widget = document.querySelector('#sidebar .panel-body');
     if (widget) {
         var widgetWidth = widget.clientWidth;
@@ -6,14 +6,62 @@ var handleResize = function (event) {
     }
 };
 
-// Don't remove this function!
-var handleReady = function () {
+function createPlayer() {
+    var musicPlayer = document.getElementsByClassName('ttw-music-player')[0];
+    if (!musicPlayer) {
+        var playlist = [
+            {
+                mp3: '/audio/1.mp3',
+                title: 'Different Colors',
+                artist: 'Walk The Moon',
+                duration: '3:42'
+            },
+            {
+                mp3: '/audio/2.mp3',
+                title: 'Cake By The Ocean',
+                artist: 'DNCE',
+                duration: '3:38'
+            },
+            {
+                mp3: '/audio/3.mp3',
+                title: 'Can\'t Stop The Feeling',
+                artist: 'Justin Timberlake',
+                duration: '3:56'
+            },
+            {
+                mp3: '/audio/4.mp3',
+                title: 'Blame It On The Girls',
+                artist: 'Mika',
+                duration: '3:18'
+            }
+        ];
+        $('#music-player-container').ttwMusicPlayer(playlist, {
+            autoPlay: false,
+            description: '',
+            jPlayer: {
+                swfPath: '/music-player/jquery-jplayer'
+            }
+        });
+    }
+};
+
+function makeLinksOpenInNewTab() {
     var elems = document.getElementsByTagName('a');
     for (var i = 0; i < elems.length; i++) {
         if (elems[i].href.length > 0 && elems[i].href.indexOf(document.domain) < 0) {
             elems[i].target = '_blank';
         }
     }
+}
+
+// Don't remove this function!
+var handleReady = function () {
+    createPlayer();
+};
+
+// Don't remove this function!
+var handleLoadedAll = function () {
+    makeLinksOpenInNewTab();
 
     // handleResize();
     // window.addEventListener('resize', handleResize);
--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ const App = Vue.extend({
 });
 
 App.component('sidebar-content', {
-    template: '<div class="panel panel-primary">\n  <div class="panel-body">\n    <h4>\n      Richard Chien<br>\n      <small>richardchien</small>\n    </h4>\n    <p>GitHub: <a href="http://github.com/richardchien">richardchien</a></p>\n    <p>Telegram: <a href="https://telegram.me/richardchien">@richardchien</a></p>\n    <p>这里是我的博客，欢迎你的来访，我会在这里写一些技术笔记和感想。</p>\n    <p>除非特别说明，文章均默认采用\n      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/" target="_blank">CC BY-SA 4.0 协议</a>。\n    </p>\n  </div>\n</div>\n<div class="panel panel-primary">\n  <div class="panel-body">\n    <h4>\n      友情链接<br>\n      <small>friend links</small>\n    </h4>\n    <ul>\n      <li><a href="http://tong-kuo.tumblr.com/">Tong Kuo</a></li>\n      <li><a href="https://www.puteulanus.com/">创世神域</a></li>\n      <li><a href="https://blog.ntzyz.io/">namespace ntzyz;</a></li>\n      <li><a href="http://www.orangejoy.moe/">404 BeenFound</a></li>\n    </ul>\n  </div>\n</div>\n<!--<div class="panel panel-primary hidden-sm hidden-xs">-->\n  <!--<div class="panel-body">-->\n    <!--<h4>-->\n      <!--歌单<br>-->\n      <!--<small>song list</small>-->\n    <!--</h4>-->\n    <!--<p>喜欢？来收藏吧：<a href="http://music.163.com/#/m/playlist?id=87165194">欢快年轻积极向上的英文歌</a></p>-->\n    <!--<div class="text-center" style="width: 100%; margin: 0px -15px -15px">-->\n      <!--<iframe id="netease-player" frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=450-->\n              <!--src="http://music.163.com/outchain/player?type=0&id=436548777&auto=0&height=430"></iframe>-->\n    <!--</div>-->\n  <!--</div>-->\n<!--</div>-->'
+    template: '<div class="panel panel-primary">\n  <div class="panel-body">\n    <h4>\n      Richard Chien<br>\n      <small>richardchien</small>\n    </h4>\n    <p>GitHub: <a href="http://github.com/richardchien">richardchien</a></p>\n    <p>Telegram: <a href="https://telegram.me/richardchien">@richardchien</a></p>\n    <p>这里是我的博客，欢迎你的来访，我会在这里写一些技术笔记和感想。</p>\n    <p>除非特别说明，文章均默认采用\n      <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/" target="_blank">CC BY-SA 4.0 协议</a>。\n    </p>\n  </div>\n</div>\n<div class="panel panel-primary">\n  <div class="panel-body">\n    <h4>\n      友情链接<br>\n      <small>friend links</small>\n    </h4>\n    <ul>\n      <li><a href="http://tong-kuo.tumblr.com/">Tong Kuo</a></li>\n      <li><a href="https://www.puteulanus.com/">创世神域</a></li>\n      <li><a href="https://blog.ntzyz.io/">namespace ntzyz;</a></li>\n      <li><a href="http://www.orangejoy.moe/">404 BeenFound</a></li>\n    </ul>\n  </div>\n</div>\n<div class="panel panel-primary">\n  <div class="panel-body">\n    <h4>\n      歌单<br>\n      <small>song list</small>\n    </h4>\n    <div id="music-player-container"></div>\n    <p>喜欢？收藏完整歌单：<a href="http://music.163.com/#/m/playlist?id=87165194">欢快年轻积极向上的英文歌</a>。</p>\n  </div>\n</div>'
 });
 
 const BaseComponent = Vue.extend({
@@ -67,10 +67,10 @@ const BaseComponent = Vue.extend({
         // document.getElementsByTagName('body')[0].appendChild(s);
         // console.log(document.getElementsByClassName('col-md-8')[0].innerHTML);
         // console.log(document.querySelector('.col-md-8'));
-        // handleReady();
+        handleReady();
         this.$watch('$loadingRouteData', function (newVal, oldVal) {
             if (oldVal === true && newVal === false) {
-                handleReady();
+                handleLoadedAll();
             }
         })
     },
